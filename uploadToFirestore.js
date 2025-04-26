82a840756f2490bcbfd158f58bbaea41c30f3da0
AhmedHussein222 <ahmdhussein222@gmail.com>
2025-04-26 11:08:42 +0300

    Refactor users.json

--- uploadToFirestore.js
+++ uploadToFirestore.js
@@ -2,44 +2,37 @@ const { db } = require("./firebase-admin");
 const fs = require("fs");
 
 // 1. قراءة ملف JSON
-const rawData = fs.readFileSync(
-  "./Data/Courses/Development/WebDevelopment.json"
-);
+const rawData = fs.readFileSync("./Data/Users.json");
+const usersData = JSON.parse(rawData);
 
-const coursesData = JSON.parse(rawData);
-
-// 2. رفع الدفعات (Batch Upload)
-async function uploadInBatches() {
-  const batchSize = 1; // الحد الأقصى للدفعة في Firestore
-  let batch = db.batch();
-
-  // تحديد مجموعة Firestore
-  const coursesCollection = db.collection("Courses/Development/WebDevelopment");
+// 2. رفع بيانات المستخدمين (Upload Users Data)
+async function uploadUsersData() {
   try {
-    for (let i = 0; i < 2; i++) {
-      const course = coursesData[i];
-      // Ensure content is an array of objects
-      if (Array.isArray(course.content)) {
-        course.content = course.content.map((item, index) => ({
-          ...item,
-          id: index + 1, // Add unique IDs to content objects
-        }));
-      }
+    // رفع بيانات المسؤول
+    // const adminDocRef = db.collection("users").doc("admin");
+    // await adminDocRef.set(usersData.admin["1"]);
+    // console.log("Admin Data Uploaded ✅");
 
-      const docRef = coursesCollection.doc(); // إنشاء مُعرف فريد
-      batch.set(docRef, course);
-
-      // إرسال الدفعة عند الوغول إلى الحد الأقصى
-      if ((i + 1) % batchSize === 0 || i === coursesData.length - 1) {
-        await batch.commit();
-        console.log(`Uplaoded Done ${Math.ceil((i + 1) / batchSize)}`);
-        batch = db.batch(); // إعادة تهيئة الدفعة
-      }
+    // رفع بيانات المدربين
+    const instructorsCollectionRef = db
+      .collection("users")
+      .collection("instructors")
+    for (const [id, instructor] of Object.entries(usersData.instructor)) {
+      await instructorsCollectionRef.doc(id).set(instructor);
     }
-    console.log("All Courses Uploaded ✅ ");
+    console.log("Instructors Data Uploaded ✅");
+
+    // رفع بيانات الطلاب
+    const studentsCollectionRef = db
+      .collection("users")
+      .collection("students")
+    for (const [id, student] of Object.entries(usersData.students)) {
+      await studentsCollectionRef.doc(id).set(student);
+    }
+    console.log("Students Data Uploaded ✅");
   } catch (error) {
-    console.error("Error ❌:", error);
+    console.error("Error Uploading Users Data ❌:", error);
   }
 }
 
-uploadInBatches();
+uploadUsersData();
