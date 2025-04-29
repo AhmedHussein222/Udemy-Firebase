0b8df92631e71364a856cbc1667533818767fc2e
AhmedHussein222 <ahmdhussein222@gmail.com>
2025-04-30 00:08:51 +0300

    update file to upload users to firebase

--- firebase-admin.js
+++ firebase-admin.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json"); // ملف مفتاح الخدمة
+const serviceAccount = require("../serviceAccountKey.json"); // ملف مفتاح الخدمة
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
